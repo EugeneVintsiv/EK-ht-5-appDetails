9b5e54cba05d6724c985ff387ebae273470bb140
Eugene <eugene.vintsiv@gmail.com>
2018-11-21 01:27:52 +0200

    Initial Commit

---
+++ appdetails/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  appdetails
+//
+//  Created by Eugene on 21.11.18.
+//  Copyright © 2018 Eugene. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
