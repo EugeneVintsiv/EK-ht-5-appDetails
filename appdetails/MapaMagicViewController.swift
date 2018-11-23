48e3d55a2943160138e336c1fd6b6f5fda8920d5
Eugene <eugene.vintsiv@gmail.com>
2018-11-23 07:16:18 +0200

    set custom color to PNG icon from code

--- appdetails/MapaMagicViewController.swift
+++ appdetails/MapaMagicViewController.swift
@@ -7,10 +7,12 @@ import UIKit
 
 class MapaMagicViewController: UIViewController {
     @IBOutlet weak var appIcon: UIImageView!
-
+    @IBOutlet weak var downloadIcon: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         setupAppIcon()
+        downloadIcon.tintColor = UIColor(ciColor: CIColor(red: (114 / 255.0), green: (171 / 255.0), blue: (210 / 255.0)))
     }
     
     private func setupAppIcon() {
@@ -19,5 +21,4 @@ class MapaMagicViewController: UIViewController {
         appIcon.layer.borderColor = UIColor.lightGray.cgColor
     }
 
-    
 }
